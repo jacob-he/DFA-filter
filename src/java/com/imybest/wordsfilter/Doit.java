8885a6cab7c7c2393cf4fadfc4303e07449bfa58
jacob.he <jacob.he@corp.globalmarket.com>
2014-12-15 10:27:52 +0800

    first commit

---
+++ src/java/com/imybest/wordsfilter/Doit.java
@@ -0,0 +1,18 @@
+package com.imybest.wordsfilter;
+
+public class Doit {
+	
+	public String prefix(String keyword) {
+		return "*";
+		
+	}
+
+	public Object keyword(String keyword) {
+		return keyword;
+	}
+
+	public Object suffix(String keyword) {
+		return "*";
+	}
+
+}
